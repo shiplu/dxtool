a79096b089f45fbd1ab0926566155f69faca0c58
Shiplu Mokaddim <shiplu.net@gmail.com>
2011-12-19 05:30:53 +0600

    Shown how to use cache and built in patterns

--- example.php
+++ example.php
@@ -6,12 +6,28 @@ require 'WebGet.php';
 $google_new_feed = 'http://news.google.com/news?pz=1&cf=all&ned=in&hl=en&output=rss';
 
 $w = new WebGet();
+
+// using cache to prevent repetitive download
+$w->useCache = true;
+$w->cacheLocation = '/tmp';
+$w->cacheMaxAge = 3600;
+
+
+
 $content = $w->requestContent($google_new_feed);
 $dx = new DataExtractor($content); 
+
+// the text part we want to match must be in the first subpattern
 $dx->titles = '|title>([^<]+)</title|a';
 $dx->rsstitle = '|title>([^<]+)</title|';
-$data = $dx->extractArray();
+print_r($dx->extractArray());
 
-print_r($data);
+
+// use built in anchor text pattern 
+$dx->init($w->requestContent("http://shiplu.mokadd.im"));
+$dx->anchor_texts = DataExtractor::PATTERN_HTML_ANCHOR_TEXT_ALL;
+$dx->anchor_urls = DataExtractor::PATTERN_HTML_ANCHOR_URL_ALL;
+
+print_r($dx->extractArray());
 
 ?>
