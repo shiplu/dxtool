e4774b2c99280d6366b6cef432b2b9ad16e1d890
Shiplu Mokaddim <shiplu.net@gmail.com>
2013-05-29 15:23:22 +0600

    pattern updated

--- examples/craiglist.php
+++ examples/craiglist.php
@@ -8,15 +8,13 @@ $url = 'http://newyork.craigslist.org/search/jjj?addFour=part-time';
 $w = new WebGet();
 
 // using cache to prevent repetitive download
-$w->useCache = true;
-$w->cacheLocation = '/tmp';
-$w->cacheMaxAge = 3600;
+$w->setup_cache(3600, '/tmp');
 
 
 $content = $w->requestContent($url);
 $dx = new DataExtractor($content); 
 
 // the text part we want to match must be in the first subpattern
-$dx->p= '|<p>(.*?)(?=</p><p>)|a';
+$dx->p= '|</small>\s*<a[^>]+>([^<]+)<|a';
 print_r($dx->extractArray());
 ?>
